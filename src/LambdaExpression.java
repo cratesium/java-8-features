9fc41075925c5746737c78a09dfc5dbed7280f38
cratesium <cratesium@gmail.com>
2024-08-18 18:30:29 +0530

    adding files

---
+++ src/LambdaExpression.java
@@ -0,0 +1,13 @@
+public class LambdaExpression {
+//  lambda function is anonymous function with
+//    no name
+//    no return type
+//    no modifier
+
+    // can oly with use with functional interfface
+    //reduces lines of code
+    //
+    public static void main(String[] args) {
+
+    }
+}
